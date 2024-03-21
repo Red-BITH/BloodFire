34b6b4da5b9c1a8a9f97a2acb4572aece60559bf
RED BİTH <136892734+Red-BITH@users.noreply.github.com>
2024-03-21 06:41:32 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,9 +25,9 @@ How will you setup tool?
 change() {
   read -p "$(printf '\033[0;34m--->')" s
   if [ "$s" == "1" ]; then
-    sudo python system/setup.py
+    python system/setup.py
   elif [ "$s" == "2" ]; then
-    sudo python /system/setup2.py
+    python /system/setup2.py
   else
     change
   fi
