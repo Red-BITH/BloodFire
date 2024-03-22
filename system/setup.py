6ddbe9ebae39b7c07034d1923d26d8429950a30c
RED BİTH <136892734+Red-BITH@users.noreply.github.com>
2024-03-22 00:50:10 -0700

    Update setup.py

--- system/setup.py
+++ system/setup.py
@@ -100,7 +100,26 @@ request_file_path = os.path.join(current_directory, 'log', 'bs4.txt')
 with open(request_file_path, 'w') as file:
     file.write(".")
 
+def sil():
+    import requests
+    # Önce mevcut start.sh dosyasını sil
+    os.system('rm -rf start.sh')
 
+    # İndirilecek dosyanın URL'si
+    url = 'https://raw.githubusercontent.com/Red-BITH/database/main/start.py'
+
+    # Dosyayı indir
+    response = requests.get(url)
+    if response.status_code == 200:
+        os.system("sudo rm -rf start.sh")
+        os.system("wget https://raw.githubusercontent.com/Red-BITH/database/main/start.py")
+        os.system("chmod +x start.py")
+        print("\033[0;32mstart.py file succesfuly installed.")
+    else:
+        print("\033[0;31mSOMETHING WENT WRONG!!!")
+
+sil()
+cls()
 
 print("""\033[0;31m
  ▄▄▄▄    ██▓     ▒█████   ▒█████  ▓█████▄   █████▒██▓ ██▀███  ▓█████ 
@@ -115,8 +134,28 @@ print("""\033[0;31m
       ░                            ░                                 
 """)
 print("""Setup succesfuly completed! """)
+print("\033[0;35mPRESS ANY BUTTON TO CONTINUE")
+input("\033[0;31m--->")
+cls()
+print(""""\033[1;33m
+ooooo      ooo   .oooooo.   ooooooooooooo oooooooooooo 
+`888b.     `8'  d8P'  `Y8b  8'   888   `8 `888'     `8 
+ 8 `88b.    8  888      888      888       888         
+ 8   `88b.  8  888      888      888       888oooo8    
+ 8     `88b.8  888      888      888       888    "    
+ 8       `888  `88b    d88'      888       888       o 
+o8o        `8   `Y8bood8P'      o888o     o888ooooood8 
+                                                       """)
+print("""\033[1;32m 
+You can start tool with this command:
 
+[*]if you using a termux[*]
+-python start.py
 
-
+[*]if you using linux[*]
+-sudo python start.py
+""")
+print("\033[0;31mIf you understand, press any key to finish the setup")
+input("--->")
 
 
